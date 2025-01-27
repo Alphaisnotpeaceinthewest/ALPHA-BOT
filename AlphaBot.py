822966121d2f28a3ddb5b4f390a83b97ea43590b
Alpha <102369274+Alphaisnotpeaceinthewest@users.noreply.github.com>
2025-01-27 10:59:43 +0200

    AlphaBot.py

--- AlphaBot.py
+++ AlphaBot.py
@@ -5,9 +5,9 @@ import os
 from flask import Flask
 import threading
 import time
+import random
 
 
-# Bot setup
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
@@ -23,7 +23,7 @@ if not TOKEN:
 else:
     print("Token loaded successfully.")
 
-# Flask app setup for handling HTTP requests
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -34,34 +34,41 @@ def home():
 def favicon():
     return send_from_directory(os.getcwd(), 'favicon.ico', mimetype='image/x-icon')
 
+
 def run_flask():
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
-# Bot event setup
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Communities"))
     print(f'Logged in as {bot.user}!')
 
-# Running Flask server on a separate thread to avoid blocking the bot
+
 def start_flask_thread():
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
-# Commands for the bot
-@bot.command()
-async def cmds(hlp):
-    help_text = (
-    "**Available Commands:**\n"
-    "`!cmds` - Displays this help message.\n"
-    "`!clear <number>` - Clears messages.\n"
-    "`!dm` - Sends a DM to all members (owner only).\n"
-    "`!ban (mention_member) (reason)` - Bans a member.\n"
-    "`!unban` - Unbans a member\n"
-    )
-    await hlp.send(help_text)
 
+
+
+user_id = 850383728022519859
+
+@bot.command()
+async def dm(ctx, *, message: str):
+    if ctx.author.id == user_id:
+     for member in ctx.guild.members:
+            try:
+              await member.create_dm()  
+              await member.dm_channel.send(message)
+            except Exception as e:
+                await ctx.author.send("Message was failed sended to some users in the server")
+     await ctx.author.send("Function Was Running")
+    else:
+        await ctx.send("You dont have a fucking permmission")
+  
+   
 
 @bot.command()
 @commands.has_permissions(manage_messages=True)
@@ -72,7 +79,7 @@ async def clear(clr, amount: int):
         await clr.send("Unknown Message was failed deleted")
 
     
-
+    
 Alpha_id = 850383728022519859
 
 @bot.command()
@@ -86,18 +93,18 @@ async def dconfig(cfn):
     
 
 
-
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
     guild = ctx.guild
     await member.ban(reason=reason)
     if reason == None:
-        reason=("For No Reason")
+        reason = "No reason was provide"
+        return
+    
     await member.send(f"You was banned from **{guild.name}** for {reason}")
     await ctx.send("Member was banned")
     
-
 
 @bot.command()
 @commands.has_permissions(ban_members=True)
@@ -108,6 +115,19 @@ async def unban(ctx, user_id: int):
         await ctx.author.send(f"{user.mention} is unbanned")
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
+
+
+@bot.command()
+async def cmds(hlp):
+        help_text = (
+        "**Available Commands:**\n"
+        "`!cmds` - Displays this help message.\n"
+        "`!clear <number>` - any amount.\n"
+        "`!dm` - !dm (mention user) only works for some roles.\n"
+        "`!ban (mention_member) (reason)` - Banning any member in the server, using !ban\n"
+        "`!unban` - unban (userid)\n"
+    )
+        await hlp.send(help_text)
 
 
 
@@ -145,7 +165,19 @@ async def on_message(message):
 
 
 
+
+color_random = [
+    3092790, 16777215, 0, 8421504, 16711680, 9109504, 65280,
+    25600, 255, 139, 65535, 32896, 16776960, 16766720,
+    16753920, 8388736, 16711935, 16761035, 10824234, 11393254,
+    65407, 15132410, 5793266, 5763719, 16705372, 15323838, 15548997
+]
+
+colorget = random.choice(color_random)
+
 channel_id = 1307762877872083035
+
+im_url = "https://cdn.discordapp.com/attachments/1294007395747500064/1304454016826081332/Screenshot_1.png?ex=677fde79&is=677e8cf9&hm=f7a24ca9173611d109eb984334fa7b1e96a88c090ee42bc72b492cb2d73c853c&"
 
 @bot.event
 async def on_member_join(member):
@@ -155,30 +187,33 @@ async def on_member_join(member):
         await member.send("``Please verify the requirements``")
         await member.send("``يرجى التحقق من المتطلبات``")
 
-        welcome_message = (
-            f"```Welcome to {guild.name}```{member.mention}\n"
-)
+        embed = discord.Embed(title=f"**WELCOME TO {guild.name}**", description=f"**Member Joined:** {member.mention}",  color=colorget)
+        embed.set_footer(text="Don't forgot to read the rules", icon_url=im_url)
+
+
+        time.sleep(0.50)
+
 
         room = bot.get_channel(channel_id)
         if room:
-            await room.send(f"Member has joined the server {member.name}")
-
-        await channel.send(welcome_message)
-        print("New Member Joined")
+            await room.send(f"@here\n**Member has joined the server**\n\n**Member User:** {member.name}\n**Member Userid:** {member.id}")
+        await channel.send(embed=embed)
 
 
 
-blacklist_two = ["583636893401612319"]
+blacklist_two = ["583636893401612319", ]
 
 ROLE_ID = 1246895667381801010
+
+role_2_id = 1326617591665655818
 
 @bot.command()
 async def verify(vm): 
     guild = vm.guild
-    role = guild.get_role(ROLE_ID)
+    role2 = guild.get_role(ROLE_ID)
     member = vm.author  
 
-    if role in member.roles:
+    if role2 in member.roles:
      await vm.send("``You are already verified, you cannot be verified again``")
      return
 
@@ -194,11 +229,50 @@ async def verify(vm):
     await vm.send("Checking User Info...")
     time.sleep(3)
     await vm.send("Roles added!")
-    await vm.author.add_roles(role)
+    await vm.author.add_roles(role2)
+
+    if role_2_id in member.roles:
+        await member.remove_roles(role_2_id)
+        return
+
+
+verification = "Enabled"
+
+RANKID = 1325100668373041257
+
+serverid = 1317542281800057014
+
+@bot.command()
+async def vr(ctx):
+ guild = ctx.guild
+ rank = guild.get_role(RANKID)
+ member = ctx.author
+ if guild.id == serverid:
+     if verification == "Enabled":
+      if rank in member.roles:
+        await ctx.send("You already have been verified")
+        return
+        
+ try:
+    if guild.id == serverid:
+       await ctx.send("Checking if verification is enabled...")
+       time.sleep(1)
+       await ctx.send("Verification is enabled")
+       time.sleep(1)
+       await ctx.send("Checking Roles..")
+       time.sleep(1)
+       await ctx.send("Welcome To Managers (OFFICIAL 2)")
+       await ctx.send("You're roles were added")
+       await ctx.author.add_roles(rank)
+
+ except Exception as e:
+    await ctx.send("ERROR Found, While Running Command")
+ except PermissionError as e:
+    await ctx.send("I don't have permission to give roles, high my role")
 
 
 
-# Running both Flask and Discord Bot
+
 async def main():
     # Start Flask server in a separate thread
     start_flask_thread()
