0d085728ba867d2f3b699b2569e8d0af30772329
Alpha <102369274+Alphaisnotpeaceinthewest@users.noreply.github.com>
2025-01-27 15:10:20 +0200

    AlphaBot.py

--- AlphaBot.py
+++ AlphaBot.py
@@ -44,10 +44,3 @@ async def main():
 # Only run if this file is executed directly
 if __name__ == "__main__":
     asyncio.run(main())
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    
-    loop.run_until_complete(main())
